930dc5812fcf63d622bb08502cd93f4c5480fa1f
jake-waratahs <jake-waratahs@users.noreply.github.com>
2014-08-20 17:56:04 +1000

    fixed leading '0', set up date

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@ static void update_time(void);
 static Window *s_main_window;
 static TextLayer *s_text_layer;
 static TextLayer *s_time_layer;
+//static TextLayer *s_date_layer;
 
 
 //Main function
@@ -36,7 +37,8 @@ static void init (void){
     .load = main_window_load,
     .unload = main_window_unload
   });
-
+   
+ 
   //I think this pushes onto a stack
   //It actually gets the watch to display the window
   window_stack_push(s_main_window, true);
@@ -45,7 +47,8 @@ static void init (void){
 }
 
 static void deinit(void){
-  //free()
+  tick_timer_service_unsubscribe();
+   //free()
   window_destroy(s_main_window);
 }
 
@@ -67,16 +70,28 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
 
   //Now do the time layer
-   
+
   s_time_layer = text_layer_create(GRect(0,100,144,68));
   
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorClear);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GAlignBottom);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
    
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+   
+  //Now the date layer
+   /*
+  s_date_layer = text_layer_create(GRect(0,129,144,39));
   
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(s_date_layer, GAlignBottom);
+  text_layer_set_text(s_date_layer, "Wednesday September 20th");
+   
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));  
+ */
 }
 
 
@@ -84,22 +99,26 @@ static void main_window_unload(Window *window) {
   //free()
   text_layer_destroy(s_text_layer);
   text_layer_destroy(s_time_layer);
+  //text_layer_destroy(s_date_layer);
 }
 
 static void update_time(void){
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   
-  static char buffer[] = "00:00";
+  static char time_buffer[] = "00:00";
  
   if (clock_is_24h_style() == true){
-     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+     strftime(time_buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
-     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+     strftime(time_buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
 
-  text_layer_set_text(s_time_layer, buffer); 
-   
+  if ((clock_is_24h_style() == false) && (time_buffer[0] == '0')){
+     text_layer_set_text(s_time_layer, &time_buffer[1]);
+  } else{
+     text_layer_set_text(s_time_layer, time_buffer); 
+  }
    
 }
 
